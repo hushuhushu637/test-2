af1d58e821e1e32af4676f1c509ea9d379b94710
Husnaara <hushuhushu637@gmail.com>
2024-05-05 16:43:45 +0530

    adding styles for about us page and modifying angular.json

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+// Import components from the same file or from another file within the same directory
+import { HomePageComponent } from './home-page/home-page.component';
+import { AboutUsPageComponent } from './about-us-page/about-us-page.component';
+import { ContactusPageComponent } from './contactus-page/contactus-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
+export const routes: Routes = [
+    { path: '', redirectTo: '/about', pathMatch: 'full' }, // Redirect to about page
+    { path: 'about', component: AboutUsPageComponent },
+    { path: 'contact', component: ContactusPageComponent },
+    { path: 'home', component: HomePageComponent },
+    { path: 'login', component: LoginPageComponent },
+    { path: 'register', component: RegisterPageComponent }
+];
---
+++ src/app/contactus-page/contactus-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contactus-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './contactus-page.component.html',
+  styleUrl: './contactus-page.component.css'
+})
+export class ContactusPageComponent {
+
+}
